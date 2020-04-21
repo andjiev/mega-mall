d82e6db0a184efc77d293e5c0d162b8cf0efd47d
Frose <gicevafrose@gmail.com>
2020-04-21 11:35:27 +0200

    Added containers in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ListStyle, TextStyle, FooterStyle, WrapperStyle, RowStyle } from './footer.styles';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,74 +23,78 @@ const Footer = () => {
       <FooterStyle>
         <section className="footer">
           <WrapperStyle>
-            <Grid container spacing={3}>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <li>
-                    ВЕБСПОТ | ТА ВЕБСПОТ ДОО <br></br>Лиценца А, рег. бр. 13-753/2
-                    <br></br> ул. Лермонтова 3/6,<br></br> 1000 Скопје
-                  </li>
-                </ListStyle>
+            <Container>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <li>
+                      ВЕБСПОТ | ТА ВЕБСПОТ ДОО <br></br>Лиценца А, рег. бр. 13-753/2
+                      <br></br> ул. Лермонтова 3/6,<br></br> 1000 Скопје
+                    </li>
+                  </ListStyle>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <h5>Технологија</h5>
+                    <li>Компјутери</li>
+                    <li>Компјутерска опрема</li>
+                    <li>Мобилни телефони</li>
+                  </ListStyle>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <h5>Мода</h5>
+                    <li>Машка облека</li>
+                    <li>Женска облека</li>
+                    <li>Обувки</li>
+                  </ListStyle>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <h5>Домакинство</h5>
+                    <li>Апарати за домаќинство</li>
+                    <li>Бела техника</li>
+                    <li>Дом и градина</li>
+                  </ListStyle>
+                </Grid>
               </Grid>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <h5>Технологија</h5>
-                  <li>Компјутери</li>
-                  <li>Компјутерска опрема</li>
-                  <li>Мобилни телефони</li>
-                </ListStyle>
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <h5>Мода</h5>
-                  <li>Машка облека</li>
-                  <li>Женска облека</li>
-                  <li>Обувки</li>
-                </ListStyle>
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <h5>Домакинство</h5>
-                  <li>Апарати за домаќинство</li>
-                  <li>Бела техника</li>
-                  <li>Дом и градина</li>
-                </ListStyle>
-              </Grid>
-            </Grid>
+            </Container>
 
-            <Grid container spacing={3}>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <h5>Следете не</h5>
-                  <li>Facebook</li>
-                  <li>Instagram</li>
-                  <li>LinkedIn</li>
-                </ListStyle>
+            <Container>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <h5>Следете не</h5>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>LinkedIn</li>
+                  </ListStyle>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <h5>Спорт</h5>
+                    <li>Машка облека</li>
+                    <li>Женска облека</li>
+                    <li>Спортска опрема</li>
+                  </ListStyle>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <h5>Убавина и здравје</h5>
+                    <li>Козметика</li>
+                    <li>Парфеми</li>
+                  </ListStyle>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                  <ListStyle>
+                    <h5>Услуги</h5>
+                    <li>Храна и пијалоци</li>
+                    <li>Едукација</li>
+                    <li>Култура и настани</li>
+                  </ListStyle>
+                </Grid>
               </Grid>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <h5>Спорт</h5>
-                  <li>Машка облека</li>
-                  <li>Женска облека</li>
-                  <li>Спортска опрема</li>
-                </ListStyle>
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <h5>Убавина и здравје</h5>
-                  <li>Козметика</li>
-                  <li>Парфеми</li>
-                </ListStyle>
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <ListStyle>
-                  <h5>Услуги</h5>
-                  <li>Храна и пијалоци</li>
-                  <li>Едукација</li>
-                  <li>Култура и настани</li>
-                </ListStyle>
-              </Grid>
-            </Grid>
+            </Container>
 
             <hr></hr>
             <TextStyle>
