f2e512e9f3a5808b8787360205de1f7c79976bd7
Frose <gicevafrose@gmail.com>
2020-04-29 13:48:12 +0200

    Added translations on main and categoty grid

--- src/pages/main/components/category-grid/category-grid.data.ts
+++ src/pages/main/components/category-grid/category-grid.data.ts
@@ -6,55 +6,54 @@ import DomakinstvoImg from '../../../../assets/images/Domakinstvo.jpg';
 import UslugiImg from '../../../../assets/images/Uslugi.jpg';
 
 import { translate } from 'lib/translate';
-import { CategoryTypes } from 'lib/enums';
 
 const cards = [
   {
     id: 1,
-    title: 'Технологија',
-    link1: 'КОМПЈУТЕРИ',
-    link2: 'КОМПЈУТЕРСКА ОПРЕМА',
-    link3: 'МОБИЛНИ ТЕЛЕФОНИ',
+    title: translate('EasyFind_CategotyGrid_Technology', 'Технологија'),
+    link1: translate('EasyFind_CategotyGrid_Link1', 'КОМПЈУТЕРИ'),
+    link2: translate('EasyFind_CategotyGrid_Link2', 'КОМПЈУТЕРСКА ОПРЕМА'),
+    link3: translate('EasyFind_CategotyGrid_Link3', 'МОБИЛНИ ТЕЛЕФОНИ'),
     img: TehnologijaImg
   },
   {
     id: 2,
-    title: 'Мода',
-    link1: 'МАШКА ОБЛЕКА',
-    link2: 'ЖЕНСКА ОБЛЕКА',
-    link3: 'ОБУВКИ',
+    title: translate('EasyFind_CategotyGrid_Fashion', 'Мода'),
+    link1: translate('EasyFind_CategotyGrid_Male', 'МАШКА ОБЛЕКА'),
+    link2: translate('EasyFind_CategotyGrid_Female', 'ЖЕНСКА ОБЛЕКА'),
+    link3: translate('EasyFind_CategotyGrid_Shoes', 'ОБУВКИ'),
     img: ModaImg
   },
   {
     id: 3,
-    title: 'Убавина и здравје',
-    link1: 'КОЗМЕТИКА',
-    link2: 'ПАРФЕМИ',
-    link3: 'НЕГА',
+    title: translate('EasyFind_CategotyGrid_Beauty', 'Убавина и здравје'),
+    link1: translate('EasyFind_CategotyGrid_Cosmetics', 'КОЗМЕТИКА'),
+    link2: translate('EasyFind_CategotyGrid_Perfumes', 'ПАРФЕМИ'),
+    link3: translate('EasyFind_CategotyGrid_Care', 'НЕГА'),
     img: UbavinaImg
   },
   {
     id: 4,
-    title: 'Спорт',
-    link1: 'СПОРТСКА ОПРЕМА',
-    link2: 'СПОРТСКА ОБЛЕКА',
-    link3: 'ФИТНЕС',
+    title: translate('EasyFind_CategotyGrid_Sport', 'Спорт'),
+    link1: translate('EasyFind_CategotyGrid_Equipment', 'СПОРТСКА ОПРЕМА'),
+    link2: translate('EasyFind_CategotyGrid_Clothes', 'СПОРТСКА ОБЛЕКА'),
+    link3: translate('EasyFind_CategotyGrid_Fitness', 'ФИТНЕС'),
     img: SportImg
   },
   {
     id: 5,
-    title: 'Домаќинство',
-    link1: 'АПАРАТИ ЗА ДОМАЌИНСТВО',
-    link2: 'БЕЛА ТЕХНИКА',
-    link3: 'ДОМ И ГРАДИНА',
+    title: translate('EasyFind_CategotyGrid_Household', 'Домаќинство'),
+    link1: translate('EasyFind_CategotyGrid_House_Equipment', 'АПАРАТИ ЗА ДОМАЌИНСТВО'),
+    link2: translate('EasyFind_CategotyGrid_White', 'БЕЛА ТЕХНИКА'),
+    link3: translate('EasyFind_CategotyGrid_Garden', 'ДОМ И ГРАДИНА'),
     img: DomakinstvoImg
   },
   {
     id: 6,
-    title: 'Услуги',
-    link1: 'ХРАНА И ПИЈАЛОЦИ',
-    link2: 'ЕДУКАЦИЈА',
-    link3: 'КУЛТУРА И НАСТАНИ',
+    title: translate('EasyFind_CategotyGrid_Services', 'Услуги'),
+    link1: translate('EasyFind_CategotyGrid_Food', 'ХРАНА И ПИЈАЛОЦИ'),
+    link2: translate('EasyFind_CategotyGrid_Education', 'ЕДУКАЦИЈА'),
+    link3: translate('EasyFind_CategotyGrid_Events', 'КУЛТУРА И НАСТАНИ'),
     img: UslugiImg
   }
 ];
