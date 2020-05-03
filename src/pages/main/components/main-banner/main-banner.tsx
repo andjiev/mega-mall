0ce0bf0d72ee01034566570e133b5cf63530cf06
Frosina7 <gicevafrose@gmail.com>
2020-05-03 22:06:07 +0200

    Frosina/main banner (#37)
    * Main Banner Finished
    
    * Main Banner final touches

--- src/pages/main/components/main-banner/main-banner.tsx
+++ src/pages/main/components/main-banner/main-banner.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Grid, Card, CardMedia, Box } from '@material-ui/core';
+import { MainBannerImage } from './main-banner.styles';
+import { mainbanner } from './main-banner.data';
 
 const MainBanner = () => {
-  return <></>;
+  return (
+    <>
+      <Box mb={5} mt={5}>
+        <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+          {mainbanner.map(res => (
+            <Grid item key={res.id} xs={8} md={8} lg={8}>
+              <Card>
+                <CardMedia>
+                  <MainBannerImage url={res.url}></MainBannerImage>
+                </CardMedia>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default MainBanner;
