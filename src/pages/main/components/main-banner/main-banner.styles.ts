d19bf67987903b02251e58bb99fddaa29e8f8f28
Frose <gicevafrose@gmail.com>
2020-05-03 19:06:40 +0200

    Fixed image on main banner

--- src/pages/main/components/main-banner/main-banner.styles.ts
+++ src/pages/main/components/main-banner/main-banner.styles.ts
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
 import { Box, Grid } from '@material-ui/core';
+import { mainbanner } from './main-banner.data';
 
-interface IGiftCardImage {
+interface IMainBanner {
   url: string;
 }
 const MainBannerImage = styled(Box)`
-  background-image: url(${(props: IGiftCardImage) => props.url});
+  background-image: url(${(props: IMainBanner) => props.url});
+  background-repeat: no-repeat;
   background-size: cover;
+  height: 350px;
   width: 100%;
-  height: 320px;
 
   ${props => props.theme.breakpoints.down('md')} {
     height: 200px;
--- src/pages/main/components/main-banner/main-banner.tsx
+++ src/pages/main/components/main-banner/main-banner.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Grid, Card, CardMedia } from '@material-ui/core';
+import { Grid, Card, CardMedia, Box } from '@material-ui/core';
 import { MainBannerImage } from './main-banner.styles';
 import { mainbanner } from './main-banner.data';
 
-const MainBanner = () => {
+interface MainBanner {
+  url: string;
+}
+
+const MainBanner = (props: MainBanner) => {
   return (
     <>
-      <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
-        <Grid item xs={12} md={6} lg={6}>
-          <Card>
-            <CardMedia>
-              <MainBannerImage url={props.url}></MainBannerImage>
-            </CardMedia>
-          </Card>
+      <Box mb={5} mt={5}>
+        <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+          <Grid item xs={8} md={8} lg={8}>
+            <Card>
+              <CardMedia>
+                <MainBannerImage url={mainbanner[0].url}></MainBannerImage>
+              </CardMedia>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </>
   );
 };
