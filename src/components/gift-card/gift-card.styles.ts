b230f57714fa4cb542ae7aa6d5365fafcfcbde4a
Frose <gicevafrose@gmail.com>
2020-05-03 13:14:26 +0200

    Added styles in gift card

---
+++ src/components/gift-card/gift-card.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+import { Box } from '@material-ui/core';
+
+interface IStyledImage {
+  url: string;
+}
+const StyledImage = styled(Box)`
+  background-image: url(${(props: IStyledImage) => props.url});
+  background-size: cover;
+  width: 100%;
+  height: 550px;
+
+  ${props => props.theme.breakpoints.down('md')} {
+    height: 350px;
+  }
+`;
+
+export { StyledImage };
--- src/components/gift-card/gift-card.tsx
+++ src/components/gift-card/gift-card.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { StyledImage } from 'components/card/card.styles';
+import { Card, Box, Typography, Hidden, Grid, CardMedia } from '@material-ui/core';
 
 interface GiftCardProps {}
 
 const GiftCard = (props: GiftCardProps) => {
-  return <></>;
+  return (
+    <>
+      <Card raised>
+        <CardMedia>
+          <StyledImage></StyledImage>
+        </CardMedia>
+      </Card>
+    </>
+  );
 };
 
 export default GiftCard;
