481024c98e415567e460f2293eb91ea93aca6a6a
Frose <gicevafrose@gmail.com>
2020-05-03 18:29:38 +0200

    Added main banner picture

--- src/pages/main/components/main-banner/main-banner.tsx
+++ src/pages/main/components/main-banner/main-banner.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Grid, Card } from '@material-ui/core';
 
 const MainBanner = () => {
-  return <></>;
+  return (
+    <>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+        <Grid item xs={12} md={6} lg={6}></Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default MainBanner;
