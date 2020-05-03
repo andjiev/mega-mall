96982022059ffff50c73582b5f3b0ebec3dbb854
Frose <gicevafrose@gmail.com>
2020-05-03 21:53:59 +0200

    Main Banner Finished

--- src/pages/main/components/main-banner/main-banner.tsx
+++ src/pages/main/components/main-banner/main-banner.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Grid, Card, CardMedia, Box } from '@material-ui/core';
+import { MainBannerImage } from './main-banner.styles';
+import { mainbanner } from './main-banner.data';
 
 const MainBanner = () => {
-  return <></>;
+  return (
+    <>
+      <Box mb={5} mt={5}>
+        <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+          <Grid item xs={8} md={8} lg={8}>
+            <Card>
+              <CardMedia>
+                <MainBannerImage url={mainbanner[0].url}></MainBannerImage>
+              </CardMedia>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default MainBanner;
