d4da1b5e731a8f112bdd84b0bb713d19e5264ebb
Frosina7 <gicevafrose@gmail.com>
2020-05-03 22:37:18 +0200

    Frosina/gift (#39)
    * Main Banner Finished
    
    * Main Banner final touches
    
    * Box added to gift card
    
    * Box is added in main
    
    Co-authored-by: Filip Brsjak <brsjakproductions@gmail.com>

--- src/pages/main/components/main-banner/main-banner.tsx
+++ src/pages/main/components/main-banner/main-banner.tsx
@@ -6,19 +6,17 @@ import { mainbanner } from './main-banner.data';
 const MainBanner = () => {
   return (
     <>
-      <Box mb={5} mt={5}>
-        <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
-          {mainbanner.map(res => (
-            <Grid item key={res.id} xs={8} md={8} lg={8}>
-              <Card>
-                <CardMedia>
-                  <MainBannerImage url={res.url}></MainBannerImage>
-                </CardMedia>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+        {mainbanner.map(res => (
+          <Grid item key={res.id} xs={8} md={8} lg={8}>
+            <Card>
+              <CardMedia>
+                <MainBannerImage url={res.url}></MainBannerImage>
+              </CardMedia>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 };
