cf2ad0809fdd520e4cbd6dc4dd6cc51e04a1142e
Filip Brsjak <brsjakproductions@gmail.com>
2020-05-22 09:20:56 +0200

    Product Grid Finished, Card Slider fix (#43)

--- src/pages/main/components/product-grid/product-grid.tsx
+++ src/pages/main/components/product-grid/product-grid.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Box, Grid, Typography, Link, Hidden } from '@material-ui/core';
+import Card from './../../../../components/card/card';
+import { cards } from './product-grid.data';
 
 const ProductGrid = () => {
-  return <></>;
+  return (
+    <>
+      <Box>
+        <Grid container justify="space-between">
+          <Grid item sm={6} xs={12}>
+            <Typography variant="h5">Најнови производи</Typography>
+          </Grid>
+          <Hidden xsDown>
+            <Grid container item xs={6} justify="flex-end" alignItems="flex-end">
+              <Box component="span">прикажи ги сите {' >'}</Box>
+            </Grid>
+          </Hidden>
+
+          <Hidden smUp>
+            <Grid container item xs={12} alignItems="flex-end">
+              <Box component="span" mt={1}>
+                прикажи ги сите {' >'}
+              </Box>
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Box>
+      <Box mt={3} pb={3}>
+        <Grid container direction="row" justify="center" alignItems="stretch" spacing={2}>
+          {cards.map(res => (
+            <Grid item key={res.id} xs={12} md={3}>
+              <Link href={res.url}>
+                <Card key={res.id} title={res.title} url={res.url} size="small">
+                  <span>{res.price}</span>
+                </Card>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default ProductGrid;
