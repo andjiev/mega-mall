037600754901371a813ef4476e6ca6664411527b
Frose <gicevafrose@gmail.com>
2020-06-18 10:56:32 +0200

    Added navigation data file

--- src/pages/category/components/navigation/navigation.tsx
+++ src/pages/category/components/navigation/navigation.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import { StyledNavigation } from './navigation.styles';
-import { Box, List, ListItem, Typography } from '@material-ui/core';
+import { Box, List, ListItem, Typography, Checkbox, Grid } from '@material-ui/core';
 
 interface NavigationProps {}
 
 const Navigation = (props: NavigationProps) => {
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
   return (
     <>
       <StyledNavigation>
@@ -14,6 +19,17 @@ const Navigation = (props: NavigationProps) => {
             <Typography variant="h6">Технологија</Typography>
             <ListItem>Компјутери и опрема</ListItem>
             <ListItem>Мобилни телефони</ListItem>
+          </List>
+
+          <List>
+            <Typography variant="h6">Производител</Typography>
+            <Grid container alignContent="center" alignItems="center">
+              <Checkbox checked={checked} onChange={handleChange} inputProps={{ 'aria-label': 'primary checkbox' }} />
+              <Typography>HP</Typography>
+            </Grid>
+            <Checkbox checked={checked} onChange={handleChange} inputProps={{ 'aria-label': 'primary checkbox' }}>
+              Dell
+            </Checkbox>
           </List>
         </Box>
       </StyledNavigation>
