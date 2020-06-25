5f7d14c1f398a1921981d8800e3bcd1326d52729
Frose <gicevafrose@gmail.com>
2020-06-25 10:17:50 +0200

    RenderItem is imported so that we have links and logo

--- src/components/header/components/top-bar/top-bar.tsx
+++ src/components/header/components/top-bar/top-bar.tsx
@@ -8,44 +8,46 @@ import { StyledLink } from 'components/styled-link';
 import { topData } from './top-bar.data';
 
 const TopBar = () => {
-  return (
-    <>
-      <Container fixed>
-        <Box pt={2}>
-          <Grid container justify="flex-end" alignContent="flex-end" alignItems="flex-end" spacing={2}>
-            {item.links.map(val => (
-              <Grid item key={val.id}>
-                <Typography variant="h6" key={val.id} style={{ color: 'white' }}>
-                  {item.links.title}
-                </Typography>
-              </Grid>
-            ))}
+  const renderItem = (item: ITopItem) => {
+    return (
+      <>
+        <Container fixed>
+          <Box pt={2}>
+            <Grid container justify="flex-end" alignContent="flex-end" alignItems="flex-end" spacing={2}>
+              {item.links.map(val => (
+                <Grid item key={val.id}>
+                  <Typography variant="h6" key={val.id} style={{ color: 'white' }}>
+                    {item.links.title}
+                  </Typography>
+                </Grid>
+              ))}
 
-            <Grid item>
-              <LanguagePickerContainer />
+              <Grid item>
+                <LanguagePickerContainer />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-        <Box pt={3}>
-          <Grid container spacing={3}>
-            <Grid item container sm={1} justify="flex-end" alignItems="center">
-              {/* TODO: replace the whole box with logo */}
-              <Box component="span" color="white">
-                {item.header.map(val => (
-                  <Box key={val.id}>
-                    <StyledImage src={val.src}></StyledImage>
-                  </Box>
-                ))}
-              </Box>
+          </Box>
+          <Box pt={3}>
+            <Grid container spacing={3}>
+              <Grid item container sm={1} justify="flex-end" alignItems="center">
+                {/* TODO: replace the whole box with logo */}
+                <Box component="span" color="white">
+                  {item.header.map(val => (
+                    <Box key={val.id}>
+                      <StyledImage src={val.src}></StyledImage>
+                    </Box>
+                  ))}
+                </Box>
+              </Grid>
+              <Grid item sm={11}>
+                <SearchBarContainer />
+              </Grid>
             </Grid>
-            <Grid item sm={11}>
-              <SearchBarContainer />
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
-    </>
-  );
+          </Box>
+        </Container>
+      </>
+    );
+  };
 };
 
 export default TopBar;
