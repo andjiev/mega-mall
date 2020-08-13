2e1096a0364ec8af76844036ecc1555d8eed7b01
Ivan Andjiev <ivan.andjiev@hotmail.com>
2020-08-13 03:05:36 +0200

    Webpack plugin merge fix (#64)
    * Webpack plugin merge fix
    
    * yarn lock update

--- webpack.config.demo.js
+++ webpack.config.demo.js
@@ -1,14 +1,25 @@
 'use strict';
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (env, argv) => ({
   mode: 'production',
   devtool: 'source-map',
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    }),
     new webpack.DefinePlugin({
       ROOT: JSON.stringify('/'),
       API_URL: JSON.stringify('https://mega-mall-api-demo.herokuapp.com/')
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: 'public/manifest.json', to: '' },
+      { from: 'public/translations.json', to: 'static' },
+      { from: 'src/assets', to: 'assets' }
+    ])
   ]
 });
