959e37aafe0c09f58de0d4e6c8abd922f7e88470
Brsjak <brsjakproductions@gmail.com>
2020-09-02 00:40:18 +0200

    slider card last minute fix

--- src/components/slider-card/slider-card.styles.ts
+++ src/components/slider-card/slider-card.styles.ts
@@ -4,7 +4,6 @@ import { Card as MaterialCard } from '@material-ui/core';
 
 const StyledMaterialCard = styled(MaterialCard)`
   height: 350px;
-  width: 100%;
   box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.24);
   margin: 5px;
 `;
