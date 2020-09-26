03e57bb3a3e1e1be441d47564d1df7ea4edba259
Frose <gicevafrose@gmail.com>
2020-09-26 19:10:04 +0200

    Footer font is done

--- src/components/styled-link/styled-link.styles.tsx
+++ src/components/styled-link/styled-link.styles.tsx
@@ -16,14 +16,22 @@ const StyledHoverLink = styled(props => {
   width: 92px;
   color: ${(props: IStyledHoverLink) =>
     props.placeToRender === 'footer' ? `${props.theme.palette.primary.contrastText}` : props.placeToRender === 'menu' ? `${props.theme.palette.primary.light}` : `${props.theme.palette.primary.contrastText}`};
-  border-bottom: ${(props: IStyledHoverLink) => (props.placeToRender === 'card' ? `2px solid ${props.theme.palette.primary.contrastText}` : '0px')};
+  border-bottom: ${(props: IStyledHoverLink) => (props.placeToRender === 'card' ? `2px solid ${props.theme.palette.primary.contrastText}` : props.placeToRender === 'footer-link' ? `1px solid transparent` : '0px')};
   padding-bottom: ${(props: IStyledHoverLink) => (props.placeToRender === 'card' ? '5px' : '0px')};
   font-family: ${(props: IStyledHoverLink) => (props.placeToRender === 'footer' ? 'OswaldRegular, sans-serif' : '')};
   transition: 0.3s;
 
   &:hover {
-    color: ${(props: IStyledHoverLink) => (props.placeToRender === 'card' ? `${props.theme.palette.primary.contrastText}` : `${props.theme.palette.primary.main}`)};
-    border-bottom: ${(props: IStyledHoverLink) => (props.placeToRender === 'card' ? `2px solid ${props.theme.palette.secondary.main}` : '0px')};
+    color: ${(props: IStyledHoverLink) =>
+      props.placeToRender === 'card'
+        ? `${props.theme.palette.primary.contrastText}`
+        : props.placeToRender === 'footer'
+        ? `${props.theme.palette.primary.contrastText}`
+        : props.placeToRender === 'footer-link'
+        ? `${props.theme.palette.primary.contrastText}`
+        : `${props.theme.palette.primary.main}`};
+    border-bottom: ${(props: IStyledHoverLink) =>
+      props.placeToRender === 'card' ? `2px solid ${props.theme.palette.secondary.main}` : props.placeToRender === 'footer-link' ? `1px solid ${props.theme.palette.primary.contrastText}` : '0px'};
   }
 `;
 
